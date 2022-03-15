63e84787e2d81930f959249ecd4d91ae869a086e
HoangMinhFpt <65846831+HoangMinhFpt@users.noreply.github.com>
2022-03-15 20:56:10 +0700

    update coffee table

--- src/layouts/booking/data/authorsTableData.js
+++ src/layouts/booking/data/authorsTableData.js
@@ -9,69 +9,96 @@
 // import logoJira from "assets/images/small-logos/logo-jira.svg";
 // import logoInvesion from "assets/images/small-logos/logo-invision.svg";
 import axios from "axios";
+import Tooltip from "@mui/material/Tooltip";
 import MDAvatar from "components/MDAvatar";
 import MDBadge from "components/MDBadge";
 import MDBox from "components/MDBox";
+import MDButton from "components/MDButton";
 import MDTypography from "components/MDTypography";
 import { useEffect, useState } from "react";
 
-export const Author = ({ image, name, email }) => (
+export const Author = ({ image, name }) => (
   <MDBox display="flex" alignItems="center" lineHeight={1} p={1}>
     <MDAvatar src={image} name={name} size="sm" />
     <MDBox ml={2} lineHeight={1}>
       <MDTypography display="block" variant="button" fontWeight="medium">
         {name}
       </MDTypography>
-      <MDTypography variant="caption">{email}</MDTypography>
     </MDBox>
   </MDBox>
 );
-export const Job = ({ title, description }) => (
-  <MDBox lineHeight={1} textAlign="left">
-    <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
-      {title}
-    </MDTypography>
-    <MDTypography variant="caption">{description}</MDTypography>
-  </MDBox>
+// export const Job = ({ title, description }) => (
+//   <MDBox lineHeight={1} textAlign="left">
+//     <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
+//       {title}
+//     </MDTypography>
+//     <MDTypography variant="caption">{description}</MDTypography>
+//   </MDBox>
+// );
+
+// function getMajorString(majorList) {
+//   let majors = "";
+//   // eslint-disable-next-line array-callback-return
+//   majorList.map((major) => {
+//     // eslint-disable-next-line eqeqeq
+//     if (majors != "") {
+//       majors = `${majors}, ${major}`;
+//     } else {
+//       majors = major;
+//     }
+//   });
+
+//   return majors;
+// }
+export const Avatars = ({ image }) => (
+  <Tooltip placeholder="bottom">
+    <MDAvatar
+      src={image}
+      size="xs"
+      sx={{
+        border: ({ borders: { borderWidth }, palette: { white } }) =>
+          `${borderWidth[2]} solid ${white.main}`,
+        cursor: "pointer",
+        position: "relative",
+
+        "&:not(:first-of-type)": {
+          ml: -1.25,
+        },
+
+        "&:hover, &:focus": {
+          zIndex: "10",
+        },
+      }}
+    />
+  </Tooltip>
 );
 
-function getMajorString(majorList) {
-  let majors = "";
-  // eslint-disable-next-line array-callback-return
-  majorList.map((major) => {
-    // eslint-disable-next-line eqeqeq
-    if (majors != "") {
-      majors = `${majors}, ${major}`;
-    } else {
-      majors = major;
-    }
-  });
-
-  return majors;
-}
-
 export default function data() {
-  const [mentor, setMentor] = useState([]);
+  const [meeting, setMeeting] = useState([]);
   useEffect(() => {
     axios
-      .get("https://theweekendexpertise.azurewebsites.net/api/v1/mentors")
+      .get(
+        "https://theweekendexpertise.azurewebsites.net/api/v1/cafe/sessions/listRq?pageIndex=1&pageSize=5"
+      )
       .then((res) => {
-        const mentors = res.data;
-        // eslint-disable-next-line array-callback-return
-        mentors.map((item) => {
-          // eslint-disable-next-line no-param-reassign
-          item.listMajor = getMajorString(item.listMajor);
-          const day = item.birthday.split(" ")[0];
-          // eslint-disable-next-line no-param-reassign
-          item.birthday = day;
-        });
-        setMentor(mentors);
+        // const mentors = res.data;
+        // // eslint-disable-next-line array-callback-return
+        // mentors.map((item) => {
+        //   // eslint-disable-next-line no-param-reassign
+        //   item.listMajor = getMajorString(item.listMajor);
+        //   const day = item.birthday.split(" ")[0];
+        //   // eslint-disable-next-line no-param-reassign
+        //   item.birthday = day;
+        // });
+        console.log(res.data);
+        console.log(meeting);
+        setMeeting(res.data);
       })
       .catch((error) => console.log(error));
   }, []);
 
   function dataTable() {
-    return mentor.map((item, index) => ({
+    return meeting.map((item, index) => ({
       stt: (
         <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
           {index + 1}
@@ -79,25 +106,35 @@ export default function data() {
       ),
       session: (
         <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-          a
+          {item.subjectName}
         </MDTypography>
       ),
-      author: <Author image={item.image} name={item.fullname} email="" />,
-      members: <Author image={item.image} name={item.fullname} email="" />,
-      time: (
+      member: (
+        <MDBox display="flex" py={2}>
+          <Avatars image={item.listMemberImage} />
+        </MDBox>
+      ),
+      mentor: <Author image={item.mentorImage} name={item.mentorName} />,
+      price: (
         <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-          a
+          {item.price} VND
         </MDTypography>
       ),
-      phone: (
+
+      location: (
         <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-          {item.phone}
+          {item.cafeStreet}, {item.cafeDistric}
+        </MDTypography>
+      ),
+      date_time: (
+        <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+          {item.date}
         </MDTypography>
       ),
       status: (
         <MDBox ml={-1}>
           <MDBadge
-            badgeContent={item.status ? "Active" : "InActive"}
+            badgeContent={item.status ? "1" : "2"}
             color="success"
             variant="gradient"
             size="sm"
@@ -106,7 +143,8 @@ export default function data() {
       ),
       action: (
         <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-          Edit
+          <MDButton color="success">Confirm</MDButton>
+          <MDButton color="error">Cancel</MDButton>
         </MDTypography>
       ),
     }));
@@ -114,12 +152,11 @@ export default function data() {
 
   return {
     columns: [
-      { Header: "STT", accessor: "stt", align: "left" },
+      { Header: "STT", accessor: "stt", width: "5%", align: "left" },
       { Header: "Session", accessor: "session", align: "left" },
-      { Header: "Mentor", accessor: "author", width: "20%", align: "left" },
-      { Header: "Thành viên", accessor: "members", width: "20%", align: "left" },
-      { Header: "Ngày giờ", accessor: "time", align: "center" },
-      { Header: "Điện thoại", accessor: "phone", align: "center" },
+      { Header: "Mentor", accessor: "mentor", width: "20%", align: "left" },
+      { Header: "Thành viên", accessor: "member", width: "20%", align: "left" },
+      { Header: "Ngày giờ", accessor: "date_time", align: "center" },
       { Header: "Trạng thái", accessor: "status", align: "center" },
       { Header: "Thao tác", accessor: "action", align: "center" },
     ],
